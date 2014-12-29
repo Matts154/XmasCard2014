1bb8d3789a75c8ac107f9f7136b88c3862a0d5ac
Matt <bored154@gmail.com>
2014-12-28 20:44:55 -0800

    Initial release.

---
+++ Scripts/RandomAnimationStart.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomAnimationStart : MonoBehaviour {
+
+	//public string animationName;
+
+	// Use this for initialization
+	void Start () {
+		AnimationState a = this.GetComponent<Animator>().animation ["Glow"];
+		a.time = Random.Range (0.0f, a.length);
+	}
+	
+}
